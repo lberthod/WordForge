5fb436ed6e125e9536cf3ca2d039bf2de632047b
lberthod <lberthod@gmail.com>
2023-04-30 20:11:36 +0200

    Update app.py
    allow size document

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# based on this github : https://github.com/nicknochnack/Langchain-Crash-Course
 # Bring in deps
 import os 
 
@@ -17,180 +18,137 @@ os.environ['OPENAI_API_KEY'] = st.secrets["auth"]
 st.set_page_config(
    page_title="wordForge",
    page_icon="🧊",
-   
 )
 
-st.title('🔗 WORD FORGE')
-st.write(', *World!* :sunglasses: WordForge help your to create your contents about your product in your e-commerce.')
+st.title('🔗💬 WORD FORGE')
+st.write('wordForge is a AI applications tools to help your to create your contents for your product page in your ecommerce.')
 st.info('WordForge is requesting the information below in order to generate the most accurate content possible for you. All of the information is optional and helps improve the accuracy of the generated output.')
-
-
-
 st.title('Content s')
+st.info('In this  input, you will give some information about the structure and the tone of communication for the text')
 
-size = st.slider('How much paragraph do you want?', 1, 4, 2)
+language = st.radio(
+    "Which language do you want to use",
+    ('English', 'French', 'German','Italian' ))
 
-
-
-
+size = st.slider('On a scale of 1 to 4, what size description do you want?', 1, 4, 2)
 prompt_tone = st.text_area('Explain your tone of the communication') 
 
 tone_list = st.multiselect(
-    'Which tone do you use?',
-    ['Affirmative', 'Negativees', 'Interrogative', 'Exclamatory','Sarcastic', 'Formal', 'Relax' , 'Authoritative','Rational', 'Emotional'],
+    'Add some tone you will use?',
+    ['Affirmative', 'Negative', 'Interrogative', 'Exclamatory','Sarcastic', 'Formal', 'Relax' , 'Authoritative','Rational', 'Emotional'],
     ['Affirmative'])
 
-
-
-
 st.title('Explain your product')
-st.info('oki')
+st.info('In this  input, you can give information abour your product and your company to help wordForge to generate a precise text for you')
 
 prompt_title = st.text_input('Title of product') 
 prompt_explanation_product = st.text_area('Explain your product') 
 prompt_explanation_market = st.text_area('Explain the market') 
 prompt_characteristic = st.text_area('Characteristic of the product') 
 prompt_supplyer = st.text_area('Who is the supplyer') 
-prompt_USP = st.text_area('What is your USP') 
-prompt_persona = st.text_area('What is your Persona') 
+
+persona_list = st.multiselect(
+    'Add some adjectivs for your persona?',
+    ['Young', 'Active', 'Interrogative', 'Outgoing','Environmentalist', 'Loyal', 'Relax' , 'Innovative','High-income', 'Urban'],
+    ['Active'])
+
 prompt_seo= st.text_area('What is SEO keyword') 
-
-
-
 
 # Prompt templates
 title_template = PromptTemplate(
     input_variables = ['topic'], 
-    template='Ecris moi le titre du produit : {topic}'
+    template='You are an expert in communication. You need to write the title of this product : : {topic}'
 )
 
 summary_template = PromptTemplate(
-    input_variables = ['title', 'prompt_tone', 'tone_list', 'prompt_explanation_product', 'prompt_explanation_market', 'prompt_characteristic', 'prompt_seo'], 
-    template='Tu es un honnête professionnel copywriter. Tu fais des documents parfais. ecris moi le premier paragraphe de 30 mots  de la description Focus on your ideal buyer Entice with benefits Avoid "yeah,yeah" phrases ustify using superlatives Appeal to your readers imagination Cut through the rational barriers with mini-stories Seduce with sensory words Tempt with social proof de maximum 30 mots pour le  {title}  en écrivant de manière de manière {prompt_tone} en te basant sur un style d ecritre {tone_list} en sachant que l explication du produit est la suivante {prompt_explanation_product} en considérant que le marché est le suivant {prompt_explanation_market} en expliquant les caractéristique du produits suivant {prompt_characteristic} en intégrant les mots suivants {prompt_seo} au minimum 2 fois pour  pour améliorer le SEO '
-)
-
-summary2_template = PromptTemplate(
-    input_variables = ['title', 'prompt_tone', 'tone_list', 'prompt_explanation_product', 'summary'], 
-    template='Ecris moi le second paragraphe de 100 mots de la description {summary} en faisant une transition douce  pour le  {title}  en écrivant de manière de manière {prompt_tone} en te basant sur un style d ecritre {tone_list} en sachant que l explication du produit est la suivante {prompt_explanation_product} '
-)
-
-summary3_template = PromptTemplate(
-    input_variables = ['title', 'prompt_tone', 'tone_list', 'prompt_explanation_product', 'summary' , 'summary2'], 
-    template='Ecris moi la suite de la description {summary} {summary2} en faisant une transition douce  pour le  {title}  en écrivant de manière de manière {prompt_tone} en te basant sur un style d ecritre {tone_list} en sachant que l explication du produit est la suivante {prompt_explanation_product} '
-)
+    input_variables = ['title', 'prompt_tone', 'tone_list', 'prompt_explanation_product', 'prompt_explanation_market', 'prompt_characteristic', 'prompt_seo', 'prompt_supplyer'], 
+    template='You are an honest professional copywriter. You make perfect documents in flowing text. write me the 100 words description of the description Focus on your ideal buyer Entice with benefits Avoid "yeah,yeah" sentences ustify using superlatives Appeal to your readers imagination Cut through the rational barriers with mini-stories Seduce with sensory words Tempt with social proof of maximum 100 words for the {title} by writing in a manner {prompt_tone} based on a style of writing {tone_list} knowing that the explanation of the product is the following {prompt_explanation_product} considering that the market is the following {prompt_explanation_market} by explaining the characteristics of the following products {prompt_characteristic} by integrating the following words {prompt_seo} at least twice to improve SEO. supplier is {prompt_supplyer}')
 
 characteristics_template = PromptTemplate(
-    input_variables = ['title' , 'prompt_characteristic'], 
-    template='Ecris moi une liste a puce pour les caractéristique de ce produit. met en forme joliment et proprement pour le  {title}  dont les caractéristique du produits suivant {prompt_characteristic}. Tu dois écrire que sur les caractéristiques transmises '
-)
-
-competence_template = PromptTemplate(
-    input_variables = ['title'], 
-    template='En tant qu expert du sujet ecris moi les 3  competences principales que les étudiants universitaire vont acquéri en suivant le cours  : {title} . ecris une phrase explicative pour chaque '
-)
-
-evaluation_template = PromptTemplate(
-    input_variables = ['title' ], 
-    template='En tant qu expert et enseignant. Ecris moi 4 types d examen pour les etudiants universitaire pour le cours  : {title} en te basant une evaluation en pédagogie orale, ecrite, en groupe, seul, projet basé sur l acquisition des compétences '
-)
-
-scenario_template = PromptTemplate(
-    input_variables = ['title' ], 
-    template='En tant qu expert et enseignant. Ecris moi les quatres titre de lecons du cours  : {title}. Transmet une liste à puce.'
-)
-
-scenario1_template = PromptTemplate(
-    input_variables = ['title' ], 
-    template='En tant qu expert et enseignant. Ecris moi un court paragraphe sur le scenario de la première lecon du cours  : {title} en te basant sur des modalités pédagogiques variés et innovantes. C est la lecon 1 sur 4. chaque lecon dure 1h30'
-)
-scenario2_template = PromptTemplate(
-    input_variables = ['title' ], 
-    template='En tant qu expert et enseignant. Ecris moi les  trois objectifs de la première lecon du cours  : {title} . C est la lecon 1 sur 4. chaque lecon dure 1h30. ecris que trois objectifs'
-)
-scenario3_template = PromptTemplate(
-    input_variables = ['title' ], 
-    template='En tant qu expert et enseignant. Ecris moi trois et uniquement trois  modalités pédagogiques  de la première lecon du cours  : {title} en te basant sur des modalités pédagogiques variés et innovantes. C est la lecon 1 sur 4. chaque lecon dure 1h30. Il y au maximum 4 modalité pédagogique'
-)
+    input_variables = ['title' , 'prompt_characteristic','prompt_supplyer'], 
+    template='You are an honest professional copywriter. Write me a quality bulleted list for the features of this product. nicely and cleanly formatting for the {title} with the following product characteristic {prompt_characteristic}. You must write only on the transmitted characteristics. supplier is {prompt_supplyer} ')
 
 resume30_template = PromptTemplate(
-    input_variables = ['summary' ], 
-    template=' Reecris ce texte en 30 mots :  {summary} '
+    input_variables = ['summary' , 'prompt_seo', 'prompt_tone', 'tone_list'], 
+    template=' Rewrite this text in 40 words: {summary} in a professional copywriterr quality text.  Based on this {summary}. {prompt_tone} based on a style of writing {tone_list}. by integrating the following words at least 2 or 3 time : {prompt_seo} for SEO referencement '    
 )
 
+resume50_template = PromptTemplate(
+    input_variables = ['summary' , 'prompt_seo', 'prompt_tone', 'tone_list'], 
+    template=' Rewrite this text in 75 words: {summary} in a professional copywriterr quality text.  {prompt_tone} based on a style of writing {tone_list}. by integrating the following words at least 2 or 3 time : {prompt_seo} for SEO referencement   '    
+)  
+
+resume75_template = PromptTemplate(
+    input_variables = ['summary', 'prompt_seo', 'prompt_tone', 'tone_list' ], 
+    template=' Rewrite this text in 10 words: {summary} in a professional copywriterr quality text.  Based on this {summary}. {prompt_tone} based on a style of writing {tone_list}  . by integrating the following words at least 2 or 3 time : {prompt_seo} for SEO referencement '    
+)  
+
+resume100_template = PromptTemplate(
+    input_variables = ['summary', 'prompt_seo', 'prompt_tone', 'tone_list'], 
+    template=' Write the first part this text in 120 words: {summary} in a professional copywriterr quality text.  Based on this {summary}. {prompt_tone} based on a style of writing {tone_list}  . by integrating the following words at least 2 or 3 time : {prompt_seo} for SEO referencement '    
+)
+
+resume_two_100_template = PromptTemplate(
+    input_variables = ['rep4', 'summary', 'prompt_seo', 'prompt_tone', 'tone_list'], 
+    template=' Your are  a professional copywriterr quality . Write the second paragraph after this text : {rep4} . Write another kind of sentence than his a previous paragraph. Based on this summary : {summary}. {prompt_tone} based on a style of writing {tone_list}. by integrating the following words at least 2 or 3 time : {prompt_seo} for SEO referencement'    
+)
+
+resume_three_100_template = PromptTemplate(
+    input_variables = ['rep4', 'rep5', 'summary', 'prompt_seo', 'prompt_tone', 'tone_list'], 
+    template=' Your are  a professional copywriterr quality . Write another innovant and quality  paragraph after this text : {rep4}  {rep5}. Write another kind of sentence than his a previous paragraph.  Based on this summary : {summary}. {prompt_tone} based on a style of writing {tone_list}. by integrating the following words at least 2 or 3 time : {prompt_seo} for SEO referencement'    
+)
 
 # Memory 
 title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 summary_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-summary2_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-summary3_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
 characteristics_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-evaluation_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-competence_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-scenario_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-scenario1_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-scenario2_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-scenario3_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
 resume30_memory = ConversationBufferMemory(input_key='summary', memory_key='chat_history')
-#scenario4_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-
+resume50_memory = ConversationBufferMemory(input_key='summary', memory_key='chat_history')
+resume75_memory = ConversationBufferMemory(input_key='summary', memory_key='chat_history')
+resume100_memory = ConversationBufferMemory(input_key='summary', memory_key='chat_history')
+resume_two_100_memory = ConversationBufferMemory(input_key='summary', memory_key='chat_history')
+resume_three_100_memory = ConversationBufferMemory(input_key='summary', memory_key='chat_history')
 
 # Llms
 llm = OpenAI(temperature=0.9) 
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
 summary_chain = LLMChain(llm=llm, prompt=summary_template, verbose=True, output_key='summary', memory=summary_memory)
-summary2_chain = LLMChain(llm=llm, prompt=summary2_template, verbose=True, output_key='summary2', memory=summary2_memory)
-summary3_chain = LLMChain(llm=llm, prompt=summary3_template, verbose=True, output_key='summary3', memory=summary3_memory)
-characteristics_chain = LLMChain(llm=llm, prompt=characteristics_template, verbose=True, output_key='summary3', memory=characteristics_memory)
-evaluation_chain = LLMChain(llm=llm, prompt=evaluation_template, verbose=True, output_key='title', memory=evaluation_memory)
-competence_chain = LLMChain(llm=llm, prompt=competence_template, verbose=True, output_key='title', memory=competence_memory)
-scenario_chain = LLMChain(llm=llm, prompt=scenario_template, verbose=True, output_key='title', memory=scenario_memory)
-scenario1_chain = LLMChain(llm=llm, prompt=scenario1_template, verbose=True, output_key='title', memory=scenario1_memory)
-scenario2_chain = LLMChain(llm=llm, prompt=scenario2_template, verbose=True, output_key='title', memory=scenario2_memory)
-scenario3_chain = LLMChain(llm=llm, prompt=scenario3_template, verbose=True, output_key='title', memory=scenario3_memory)
+characteristics_chain = LLMChain(llm=llm, prompt=characteristics_template, verbose=True, output_key='characteristics', memory=characteristics_memory)
 resume30_chain = LLMChain(llm=llm, prompt=resume30_template, verbose=True, output_key='rep', memory=resume30_memory)
-#scenario4_chain = LLMChain(llm=llm, prompt=scenario4_template, verbose=True, output_key='title', memory=scenario4_memory)
-
+resume50_chain = LLMChain(llm=llm, prompt=resume50_template, verbose=True, output_key='rep2', memory=resume50_memory)
+resume75_chain = LLMChain(llm=llm, prompt=resume75_template, verbose=True, output_key='rep3', memory=resume75_memory)
+resume100_chain = LLMChain(llm=llm, prompt=resume100_template, verbose=True, output_key='rep4', memory=resume100_memory)
+resume100_two_chain = LLMChain(llm=llm, prompt=resume100_template, verbose=True, output_key='rep5', memory=resume100_memory)
+resume100_three_chain = LLMChain(llm=llm, prompt=resume100_template, verbose=True, output_key='rep6', memory=resume100_memory)
 
 # Show stuff to the screen if there's a prompt
 if st.button('Start running'):
 
     title = title_chain.run(prompt_title)
-    summary = summary_chain.run(title=title, prompt_tone=prompt_tone, tone_list=tone_list, prompt_explanation_product=prompt_explanation_product, prompt_explanation_market=prompt_explanation_market, prompt_characteristic=prompt_characteristic, prompt_seo=prompt_seo)
-    rep = resume30_chain.run(summary=summary)
-  
-  #  summary2 = summary2_chain.run(title=title, prompt_tone=prompt_tone, tone_list=tone_list, prompt_explanation_product=prompt_explanation_product, summary=summary)
-  #  summary3 = summary3_chain.run(title=title, prompt_tone=prompt_tone, tone_list=tone_list, prompt_explanation_product=prompt_explanation_product, summary=summary, summary2=summary2) ds
-
-   # characteristics = characteristics_chain.run(title=title,  prompt_explanation_product=prompt_explanation_product, prompt_explanation_market=prompt_explanation_market, prompt_characteristic=prompt_characteristic)
-   # evaluation = evaluation_chain.run(title=title)
-  #  competence = competence_chain.run(title=title)
-  #  scenario = scenario_chain.run(title=title)
-  #  scenario1 = scenario1_chain.run(title=title)
-  #  scenario2 = scenario2_chain.run(title=title)
- #  scenario3 = scenario3_chain.run(title=title)
-   # scenario4 = scenario4_chain.run(title=title)
-
+    st.write('Size of text ', size)
     st.info('Titre : \n' +  title)
-    st.info('Description : \n ' + summary)
-  #  st.info('Characteristics : \n' +  characteristics)
+    summary = summary_chain.run(title=title, prompt_tone=prompt_tone, tone_list=tone_list, prompt_explanation_product=prompt_explanation_product, prompt_explanation_market=prompt_explanation_market, prompt_characteristic=prompt_characteristic, prompt_seo=prompt_seo, prompt_supplyer=prompt_supplyer)
+    st.info('Main Description : \n ' + summary)
 
- 
+    if size  == 1:
+        rep = resume30_chain.run(summary=summary, prompt_seo=prompt_seo, prompt_tone=prompt_tone, tone_list=tone_list)
+        st.info('Short Description : \n ' + rep)
 
-  #  with st.expander('Evaluation du cours'): 
-  #      st.info(evaluation_memory.buffer)
+    if size  == 2:
+        rep3 = resume75_chain.run(summary=summary, prompt_seo=prompt_seo, prompt_tone=prompt_tone, tone_list=tone_list)
+        st.info('Medium Description : \n ' + rep3)
+     
+    if size  == 3:
+        rep4 = resume100_chain.run(summary=summary, prompt_seo=prompt_seo, prompt_tone=prompt_tone, tone_list=tone_list)
+        rep5 = resume100_two_chain.run(rep4=rep4 ,summary=summary, prompt_seo=prompt_seo, prompt_tone=prompt_tone, tone_list=tone_list)
+        st.info('Large Description : \n ' + rep4 + '\n\n' + rep5)
 
-  #  with st.expander('Compétence du cours'): 
- #      st.info(competence_memory.buffer)
-#
-  #  with st.expander('Résumé du scénario du cours'): 
-    #    st.info(scenario_memory.buffer)
+    if size  == 4:
+        rep4 = resume100_chain.run(summary=summary, prompt_seo=prompt_seo, prompt_tone=prompt_tone, tone_list=tone_list)
+        rep5 = resume100_two_chain.run(rep4=rep4 ,summary=summary, prompt_seo=prompt_seo, prompt_tone=prompt_tone, tone_list=tone_list)
+        rep6 = resume100_three_chain.run(rep4=rep4 , rep5=rep5 ,summary=summary, prompt_seo=prompt_seo, prompt_tone=prompt_tone, tone_list=tone_list)
+        st.info('Large Description : \n ' + rep4 + '\n\n' + rep5 + '\n\n' + rep6)
 
-   # with st.expander('Introduction de la lecon 1'): 
-   #     st.info(scenario1_memory.buffer)
-
-   # with st.expander('Objectifs de la lecon 1'): 
-  #      st.info(scenario2_memory.buffer)
-
-   # with st.expander('Modalité de la lecon 1'): 
-     #   st.info(scenario3_memory.buffer)
-
+    if prompt_characteristic  != "":
+        characteristics = characteristics_chain.run(title=title,prompt_characteristic=prompt_characteristic ,prompt_supplyer=prompt_supplyer)
+        st.info('Characterisics : \n' +  characteristics)
